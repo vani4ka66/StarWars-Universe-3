5934b78dcd5d3fc7b6c3bc014c55f153e0ce3b9a
Vanya Dimitrova <vani4ka66@abv.bg>
2021-09-11 22:45:16 +0300

    Commit 6

--- src/app/custom/StarWarsUniverse.js
+++ src/app/custom/StarWarsUniverse.js
@@ -3,7 +3,7 @@ import Entity from './Entity'
 export default class StarWarsUniverse {
     constructor() {
 
-        this._entities = [];
+        this.entities = [];
 
         this.init();
     }
