9e019db9fb58ecee110d38da951a1f2fd9d48c4c
Vanya Dimitrova <vani4ka66@abv.bg>
2021-09-12 11:12:41 +0300

    Commit 8

--- src/app/custom/Entity.js
+++ src/app/custom/Entity.js
@@ -1,20 +1,7 @@
 export default class Entity {
     constructor(name, data) {
 
-        this._name = name;
-        this._data = data
-    }
-
-    get name() {
-        return this._name;
-    }
-
-    get data() {
-        return this.data;
-    }
-
-    set(name, data) {
-        this._name = name
-        this._data = data
+        this.name = name;
+        this.data = data
     }
 }
